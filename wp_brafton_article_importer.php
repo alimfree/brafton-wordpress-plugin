29a8c4e89dd156ddeb4ce01072ea9c2372ca7bd8
Ali <alimf@bc.edu>
2014-06-18 22:26:50 -0400

    syntax error

--- wp_brafton_article_importer.php
+++ wp_brafton_article_importer.php
@@ -83,7 +83,7 @@ if( !class_exists( 'WP_Brafton_Article_Importer' ) )
                     'raw_url' => 'https://raw.github.com/username/brafton_wordpress_plugin/1.3.5', // the github raw url of your github repo
                     'github_url' => 'https://github.com/alimfree/brafton_wordpress_plugin', // the github url of your github repo
                     'zip_url' => 'https://github.com/alimfree/brafton_wordpress_plugin/zipball/1.3.5', // the zip url of the github repo
-                    'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+                    'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
                     'requires' => '3.0', // which version of WordPress does your plugin require?
                     'tested' => '3.3', // which version of WordPress is your plugin tested up to?
                     'readme' => 'README.md', // which file to use as the readme for the version number
